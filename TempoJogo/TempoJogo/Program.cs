ccf75b470591b8129185dc59a48a22b29d1552df
diegogalli <84423207+diegogalli@users.noreply.github.com>
2021-08-11 15:04:30 -0300

    Uri 1046 tempo de jogo

---
+++ TempoJogo/TempoJogo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace TempoJogo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] DuracaoJogo = Console.ReadLine().Split(' ');
+
+            int HoraInicio = int.Parse(DuracaoJogo[0]);
+            int HoraFinal = int.Parse(DuracaoJogo[1]);
+
+            int tempo;
+            
+            if ( HoraInicio < HoraFinal)
+            {
+                tempo = HoraFinal - HoraInicio;
+            }
+            else
+            {
+                tempo = 24 - HoraInicio + HoraFinal;
+            }
+            Console.WriteLine("O JOGO DUROU " + tempo + " HORA(S)");
+            
+        }
+    }
+}
